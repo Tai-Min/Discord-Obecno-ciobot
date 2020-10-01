614effba99f3897fe9939deb58454bcce171e45c
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2020-10-01 17:58:49 +0200

    Tables can now be bigger than 2000 chars

--- commands/command_table.js
+++ commands/command_table.js
@@ -13,20 +13,41 @@ class HelpCommand extends Command {
         this.descriptionString = strings.tableDescription;
     }
 
-    equalizeArrays(arr){
+    equalizeArrays(arr) {
         let maxLen = 0;
-        for(let i = 0; i < arr.length; i++){
-            if(arr[i].length > maxLen)
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].length > maxLen)
                 maxLen = arr[i].length;
         }
 
-        for(let i = 0; i < arr.length; i++){
-            arr[i].push(...Array(maxLen-arr[i].length).fill(" "));
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].push(...Array(maxLen - arr[i].length).fill(" "));
         }
     }
 
+    splitTable(tab){
+        const lines = tab.split("\n");
+
+        let cntr = 0;
+        let resArr = [];
+        while(cntr < lines.length){
+            let res = "```";
+            while(res.length + lines[cntr].length + 1 + 3 < 2000 && cntr < lines.length){
+                res += lines[cntr] + "\n";
+                cntr++;
+                if(cntr >= lines.length)
+                    break;
+            }
+            res += "```";
+
+            resArr.push(res);
+        }
+        
+        return resArr;
+    }
+
     exec(bot, msg, args) {
-        if(!this.canUseCommand(msg.member)){
+        if (!this.canUseCommand(msg.member)) {
             bot.sendLogs(name + " tried to use " + this.commandName + " but don't have permissions.");
             msg.delete();
             return false;
@@ -35,18 +56,27 @@ class HelpCommand extends Command {
         const spreadsheetId = args[0];
         const cellRange = args[1];
         const url = 'https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheetId + '/values/' + cellRange + '?key=' + config['spreadsheetsApiKey'];
-        const settings = {method : "Get"};
-        fetch(url,settings)
-        .then(res => res.json())
-        .then((json)=>{
-            this.equalizeArrays(json.values);
-            const result = "\`\`\`\n" + table(json.values) + "\n\`\`\`";
-            this.reply(msg, result);
-            
-        }).catch(()=>{
-            this.replyThenDelete(msg, strings.tableGetFail, 10000);
-            return false;
-        });
+        const settings = { method: "Get" };
+        fetch(url, settings)
+            .then(res => res.json())
+            .then((json) => {
+                this.equalizeArrays(json.values);
+
+                const result = table(json.values);
+                const resultsArr = this.splitTable(result);
+
+                let promises = [];
+                for(let i = 0; i < resultsArr.length; i++){
+                    promises.push(msg.channel.send(resultsArr[i]));
+                }
+                Promise.all(promises)
+                .then(()=>{
+                    msg.delete();
+                })
+            }).catch(() => {
+                this.replyThenDelete(msg, strings.tableGetFail, 10000);
+                return false;
+            });
     }
 }
 
